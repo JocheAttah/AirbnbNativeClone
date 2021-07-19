d448b968f52aac6df246afc1587fcb4d4f1f32cf
Attah David <jocheattahdavid@gmail.com>
2021-07-19 12:54:57 +0100

    bottom sheet fixed

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import BottomSheet, {BottomSheetScrollView} from '@gorhom/bottom-sheet';
 import {View, FlatList, Text} from 'react-native';
 import feed from '../../../assets/data/feed';
 import Post from '../../components/Post';
 
+const renderItem = ({item}) => (
+  <Post post={item} keyExtractor={item => item.id} />
+);
+
 const SearchResultsScreen = () => {
   return (
-    <View>
-      <FlatList
-        data={feed}
-        renderItem={({item}) => (
-          <Post post={item} keyExtractor={item => item.id} />
-        )}
-      />
-      {/* <Text>common</Text> */}
+    <View style={{flex: 1}}>
+      <View>
+        {/* <FlatList
+          data={feed}
+          renderItem={({item}) => (
+            <Post post={item} keyExtractor={item => item.id} />
+          )}
+        /> */}
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+        <Text>fhfjfkflflfl</Text>
+      </View>
+      <BottomSheet initialSnapIndex={0} snapPoints={['3%','50%', '100%']}>
+        {/* <BottomSheetScrollView> */}
+        <FlatList data={feed} renderItem={renderItem} />
+        {/* </BottomSheetScrollView> */}
+      </BottomSheet>
     </View>
   );
 };
